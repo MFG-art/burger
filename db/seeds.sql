0d2206a52749fc1ae3a005fcf0343cb72c700ba3
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2020-01-04 17:47:59 -0600

    Completed schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The BLT', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Three Meat Burger', false);
